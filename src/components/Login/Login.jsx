a71fe0b123ea1d722589fd63840950f27ab91f91
DanilShchepelin <d.shepelin@gmail.com>
2022-06-24 19:09:23 +0500

    all new

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,34 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './Login.css';
 
-class Login extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    // }
+function Login(props) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    render() {
-        return (
-            <div className="login_form">
-                <h2>Login</h2>
-                <form>
-                    <div className="login_user_box">
-                        <div>E-Mail</div>
-                        <input type="email" name="" required=""></input>
-                    </div>
-                    <div className="login_user_box">
-                        <div>Password</div>
-                        <input type="password" name="" required=""></input>
-                    </div>
-                    <button className="login_button">Sign in</button>
-                    <div className='login_text'>OR</div>
-                    <NavLink to={'/registration'}>
-                        <button className="login_button">Create new user</button>
-                    </NavLink>
-                </form>
-            </div>
-        );
+    const submit = () => {
+        console.log(email, password);
+        fetch('http://localhost:8080/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email, password })
+        });
     }
+    return (
+        <div className="login_form">
+            <h2>Login</h2>
+            <form>
+                <div className="login_user_box">
+                    <div>E-Mail</div>
+                    <input type="email" name="email" required="" value={email} onChange={event => setEmail(event.target.value)}></input>
+                </div>
+                <div className="login_user_box">
+                    <div>Password</div>
+                    <input type="password" name="password" required="" value={password} onChange={event => setPassword(event.target.value)}></input>
+                </div>
+                <button className="login_button" onClick={submit}>Sign in</button>
+                <div className='login_text'>OR</div>
+                <NavLink to={'/registration'}>
+                    <button className="login_button">Create new user</button>
+                </NavLink>
+            </form>
+        </div>
+    );
 }
+
 
 export default Login;
